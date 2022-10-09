ade516f30f754ee0390ccd270b5f867698da1898
Charles Day <charles.day@zynstra.com>
2022-10-09 22:52:12 +0100

    Fixed dark mode styling & mobile sizing

--- web/static/js/utils.js
+++ web/static/js/utils.js
@@ -2,7 +2,7 @@
 function mobileWidthCheck() {
 	//Checks whether the window is less then 900 pixels
 	if (window.innerWidth < 900) {
-		updateProperty('main','width', (window.innerWidth - BAR_WIDTH) + 'px')
+		updateProperty('main','width', (window.innerWidth - 20) + 'px')
 	} else {
 		updateProperty('main','width','70%')
 	}
@@ -16,16 +16,4 @@ function updateProperty(elementID, property, value) {
 
 function capitalize(string){
 	return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function submitText(text_type) {
-	input_textbox = document.getElementById('input_text');
-	if (text_type == "alert") {
-		fetch("/ink/text?style=alert&text=" + input_textbox.value);
-	} else if (text_type == "banner") {
-		fetch("/ink/text?style=banner&text=" + input_textbox.value);
-	} else {
-		fetch("/ink/text?style=splash&text=" + input_textbox.value);
-	}
-	input_textbox.value = "";
 }
