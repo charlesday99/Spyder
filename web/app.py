ca205a0958c7d66801ef9f468a238f255c3466bd
Charles Day <charles.day@zynstra.com>
2022-10-10 12:19:35 +0100

    Add dynamic page count to results page

--- web/app.py
+++ web/app.py
@@ -4,41 +4,62 @@ from indexer.core import *
 
 from flask import Flask, render_template, request, abort, send_from_directory
 from datetime import datetime
+from threading import Thread
+import time
 import os
 
 app = Flask(__name__,static_url_path='')
 db_connect()
+
+page_count = 0
+
 
 @app.route("/")
 def search():
     date = datetime.now().strftime('%A %d %B').replace(' 0', ' ')
     return render_template('search.html', date=date)
 
+
 @app.route("/search")
 def results():
+    global page_count
     query = request.args.get('q')
     page = request.args.get('page') or 0
 
     if query is not None:
         page_start = int(page)*10
         page_end = int(page)*10+10
+
         results = Page.objects.search_text(query).order_by('$text_score')[page_start:page_end]
+
         return render_template(
             'results.html',
             query=query,
-            results=results
+            results=results,
+            pages_count="{:,}".format(page_count),
         )
     else:
         abort(400)
 
+
 @app.route("/rankings")
 def rankings():
     return render_template('rankings.html')
+
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
         'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+
+def updatePageCount():
+    global page_count
+    while True:
+        page_count = Page.objects.count()
+        time.sleep(600)
+
+
 if __name__ == "__main__":
+    Thread(target=updatePageCount).start()
     app.run(host='0.0.0.0')
